31d5fe7003cf0a3163149ef924ecadca475aac08
Rohit Chormale <rohitchormale@gmail.com>
2018-09-05 21:40:12 +0530

    added exercise template

---
+++ coding-exercise/python/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+"""
+
+
+def foo(a):
+    pass
+
+
+import unittest
+class FooTestCase(unittest.TestCase):
+    def test_foo(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
